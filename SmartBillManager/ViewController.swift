d057dc6b8300d8322deee4fadc41d1995835bc91
Hamza Azam <hamzaazam14@gmail.com>
2020-04-21 12:26:49 +0500

    Initial Commit

---
+++ SmartBillManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartBillManager
+//
+//  Created by Hamza Azam on 21/04/2020.
+//  Copyright © 2020 Hamza Azam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
