6092f9b814602102a3f7185412a588c82d859c15
Hamza Azam <hamzaazam14@gmail.com>
2020-05-31 13:13:49 +0500

    Bill Image Can now be viewed and zoomed

--- SmartBillManager/Controller/BillImageViewController.swift
+++ SmartBillManager/Controller/BillImageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class BillImageViewController: UIViewController {
     
@@ -14,26 +15,56 @@ class BillImageViewController: UIViewController {
         didSet{
         }
     }
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     var index: Int?
     var displayText: String?
     @IBOutlet weak var ivBillImage: UIImageView!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         displayLabel.text = displayText
         
+        ivBillImage.image = selectedBill?.billImage
+        
+        scrollView.maximumZoomScale = 4
+        scrollView.minimumZoomScale = 1
+        scrollView.delegate = self
+
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension BillImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return ivBillImage
     }
-    */
-
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            if let image = ivBillImage.image {
+                let ratioW = ivBillImage.frame.width / image.size.width
+                let ratioH = ivBillImage.frame.height / image.size.height
+                
+                let ratio = ratioW < ratioH ? ratioW : ratioH
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                let conditionLeft = newWidth*scrollView.zoomScale > ivBillImage.frame.width
+                let left = 0.5 * (conditionLeft ? newWidth - ivBillImage.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                let conditioTop = newHeight*scrollView.zoomScale > ivBillImage.frame.height
+                
+                let top = 0.5 * (conditioTop ? newHeight - ivBillImage.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+                
+            }
+        } else {
+            scrollView.contentInset = .zero
+        }
+    }
 }
