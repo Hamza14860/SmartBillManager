2ec82c83faa1c09f6d6674705a1681090d269673
Hamza Azam <hamzaazam14@gmail.com>
2020-06-08 22:26:57 +0500

    Forget Password Working

--- SmartBillManager/Controller/ForgetPassScreenViewController.swift
+++ SmartBillManager/Controller/ForgetPassScreenViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ForgetPassScreenViewController: UIViewController {
 
@@ -16,15 +17,25 @@ class ForgetPassScreenViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var emailToReset: UITextField!
+    
+    @IBAction func btnResetPressed(_ sender: UIButton) {
+        if let email = emailToReset.text {
+            Auth.auth().sendPasswordReset(withEmail: email, completion: { (error) in
+                       if error != nil{
+                           let resetFailedAlert = UIAlertController(title: "Reset Failed", message: "Error: \(String(describing: error?.localizedDescription))", preferredStyle: .alert)
+                           resetFailedAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                           self.present(resetFailedAlert, animated: true, completion: nil)
+                       }else {
+                           let resetEmailSentAlert = UIAlertController(title: "Reset email sent successfully", message: "Check your email", preferredStyle: .alert)
+                           resetEmailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                           self.present(resetEmailSentAlert, animated: true, completion: nil)
+                       }
+                   })
+        }
+        
     }
-    */
+    
+    
 
 }
